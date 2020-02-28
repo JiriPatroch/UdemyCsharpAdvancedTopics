93574d84f1e24ff8be173106eea210c36f54cffc
DIGI-DEER <jiripatroch@gmail.com>
2020-02-28 23:07:32 +0100

    Added - Section 8

---
+++ Section8_Asynchronous_programming/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section8_Asynchronous_programming {
+
+	internal class Program {
+
+		private static void Main(string[] args) {
+			int count = 200000;
+			char charToConcatenate = '1';
+
+			//Async that can return a value
+			Task<string> t = ConcatenateCharsAsync(charToConcatenate, count);
+
+			Console.WriteLine("In progress");
+
+			Console.WriteLine("The length of the result is " + t.Result.Length);
+			Console.ReadKey();
+		}
+
+		private static string ConcatenateChars(char charToConcatenate, int count) {
+			string concatenatedString = string.Empty;
+
+			for(int i = 0; i < count; i++) {
+				concatenatedString += charToConcatenate;
+			}
+
+			return concatenatedString;
+		}
+
+		private async static Task<string> ConcatenateCharsAsync(char charToConcatenate, int count) {
+			return await Task<string>.Factory.StartNew(() => ConcatenateChars(charToConcatenate, count));
+		}
+	}
+}
